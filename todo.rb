140ea12cea0a1d7e6f4d3f84a940cadeb4a18e90
Simon Dein <simon.dein@gmail.com>
2018-05-18 20:30:24 +0200

    Added unique identifiers to lists

--- todo.rb
+++ todo.rb
@@ -14,20 +14,65 @@ end
 
 before do
   session[:lists] ||= [
-    { name: 'Today',
-      todos: [{ id: 0, name: 'Workout', completed: false },
-              { id: 1, name: 'Do loundry', completed: false },
-              { id: 2, name: 'Eat ice cream', completed: false }] }
+    { id: 1, name: 'Today',
+      todos: [{ id: 1, name: 'Workout', completed: false },
+              { id: 2, name: 'Do loundry', completed: false },
+              { id: 3, name: 'Eat ice cream', completed: false }] }
   ]
 end
 
+#####################################################################################
+##################################### Methods #######################################
+#####################################################################################
+
 # Validating list exists - redirect if not
-def load_list(index)
-  list = session[:lists][index] if index && session[:lists][index]
+def load_list(id)
+  list = session[:lists].find { |l| l[:id] == id }
+
   return list if list
 
   session[:error] = 'The list could not be found'
   redirect '/lists'
+end
+
+# generate next id_num for todo's in todolist
+def next_id(array)
+  max = array.map { |obj| obj[:id] }.max || 0
+  max + 1
+end
+
+# Return error messsage if name is invalid - else return nil
+def list_name_error(list_name)
+  if !valid_length?(list_name)
+    'List name must be between 1 and 100 characters.'
+  elsif !valid_name?(list_name)
+    'List name must be unique.'
+  end
+end
+
+def todo_name_error(name)
+  'Todo name must be between 1 and 100 characters.' unless valid_length?(name)
+end
+
+def valid_name?(list_name)
+  session[:lists].none? { |list| list[:name] == list_name }
+end
+
+def valid_length?(list_name)
+  (1..100).cover?(list_name.size)
+end
+
+# toggles checkbox on or off for todo
+def toggle_check_box(todo)
+  case todo[:completed]
+  when true then todo[:completed] = false
+  when false then todo[:completed] = true
+  end
+end
+
+def list_completed?(list)
+  return false if list[:todos].empty?
+  list[:todos].all? { |todo| todo[:completed] == true }
 end
 
 #####################################################################################
@@ -41,7 +86,6 @@ end
 # View all the lists
 get '/lists' do
   @lists = session[:lists]
-
   erb :lists, layout: :layout
 end
 
@@ -63,23 +107,25 @@ end
 get '/lists/:list_id/edit' do
   @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
+
   erb :edit_list, layout: :layout
 end
 
 #####################################################################################
-################## Handling addeding and removing behind the scenes #################
+################### Handling adding and removing behind the scenes ##################
 #####################################################################################
 
 # Create new list
 post '/lists' do
+  lists = session[:lists]
   list_name = params[:list_name].strip
-  
+
   error = list_name_error(list_name)
   if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << { name: list_name, todos: [] }
+    session[:lists] << { id: next_id(lists), name: list_name, todos: [] }
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
@@ -87,42 +133,34 @@ end
 
 # Update a todo list name
 post '/lists/:list_id/edit' do
-  @id = params[:list_id].to_i
+  @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
   list_name = params[:list_name].strip
 
   error = list_name_error(list_name)
   if error
-    @list = session[:lists][@id]
-    @id = params[:id].to_i
     session[:error] = error
     erb :edit_list, layout: :layout
   else
     @list[:name] = list_name
     session[:success] = 'The list name has been changed.'
-    redirect "/lists/#{@id}"
+    redirect "/lists/#{@list_id}"
   end
 end
 
 # Permanently delete list
 post '/lists/:id/delete' do
   id = params[:id].to_i
-  list_name = session[:lists][id][:name]
-  session[:lists].delete_at(id)
+  list_name = session[:lists].find { |list| list[:id] == id }[:name]
+  session[:lists].delete_if { |list| list[:id] == id }
 
   # If ajax request
-  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+  if env['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
     '/lists'
   else
-    session[:success] = "The list \"#{list_name}\" has been removed"
+    session[:succes] = "The list \"#{list_name}\" has been removed"
     redirect '/lists'
   end
-end
-
-# generate next id_num for todo's in todolist
-def next_todo_id(todos)
-  max = todos.map { |todo| todo[:id] }.max || 0
-  max + 1
 end
 
 # Add todo's to to a list
@@ -137,8 +175,8 @@ post '/lists/:list_id/todos' do
     session[:error] = error
     erb :list, layout: :layout
   else
-    id = next_todo_id(@list[:todos])
-    
+    id = next_id(@list[:todos])
+
     @list[:todos] << { id: id, name: text, completed: false }
     session[:success] = 'The todo was added.'
     redirect "/lists/#{@list_id}"
@@ -180,7 +218,7 @@ post '/lists/:list_id/todos/:todo_id/delete' do
   end
 
   # If ajax request
-  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+  if env['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
     status 204
   else
     session[:success] = 'The todo has been deleted'
@@ -189,39 +227,10 @@ post '/lists/:list_id/todos/:todo_id/delete' do
 end
 
 #####################################################################################
-################################## Helper Methods ###################################
+################################ View Helper Methods ################################
 #####################################################################################
 
 helpers do
-  # Return error messsage if name is invalid - else return nil
-  def list_name_error(list_name)
-    if !valid_length?(list_name)
-      'List name must be between 1 and 100 characters.'
-    elsif !valid_name?(list_name)
-      'List name must be unique.'
-    end
-  end
-
-  def todo_name_error(name)
-    'Todo name must be between 1 and 100 characters.' unless valid_length?(name)
-  end
-
-  def valid_name?(list_name)
-    session[:lists].none? { |list| list[:name] == list_name }
-  end
-
-  def valid_length?(list_name)
-    (1..100).cover?(list_name.size)
-  end
-
-  # toggles checkbox on or off for todo
-  def toggle_check_box(todo)
-    case todo[:completed]
-    when true then todo[:completed] = false
-    when false then todo[:completed] = true
-    end
-  end
-
   def completed_todos(list)
     list[:todos].count { |todo| todo[:completed] == true }
   end
@@ -236,11 +245,6 @@ helpers do
     todo_class = []
     todo_class << 'complete' if todo[:completed] == true
     todo_class.join(' ')
-  end
-
-  def list_completed?(list)
-    return false if list[:todos].empty?
-    list[:todos].all? { |todo| todo[:completed] == true }
   end
 
   def sort_lists(lists)
